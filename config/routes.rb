78d2cc6f1c6a977bbebfc95673f75d1475d2e59b
DRpandaMD <michael.zarate.ce@gmail.com>
2016-06-28 09:21:07 -0700

    added hello

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  #added here June 28 2016
+  root 'application#hello'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
